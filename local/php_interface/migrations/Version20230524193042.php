4c04bfb0b7ab60891fc153ba723781601145db39
mkv173 <115578800+mkv173@users.noreply.github.com>
2023-05-24 20:32:51 +0400

    migration  elements

---
+++ local/php_interface/migrations/Version20230524193042.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version20230524193042 extends Version
+{
+    protected $description = "Разделы инфоблока товары";
+
+    protected $moduleVersion = "4.2.4";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+
+        $iblockId = $helper->Iblock()->getIblockIdIfExists(
+            'products',
+            'products'
+        );
+
+        $helper->Iblock()->addSectionsFromTree(
+            $iblockId,
+            array (
+  0 => 
+  array (
+    'NAME' => 'Woman\'s',
+    'CODE' => 'womans',
+    'SORT' => '300',
+    'ACTIVE' => 'Y',
+    'XML_ID' => NULL,
+    'DESCRIPTION' => '',
+    'DESCRIPTION_TYPE' => 'text',
+  ),
+  1 => 
+  array (
+    'NAME' => 'Men\'s',
+    'CODE' => 'mens',
+    'SORT' => '400',
+    'ACTIVE' => 'Y',
+    'XML_ID' => NULL,
+    'DESCRIPTION' => '',
+    'DESCRIPTION_TYPE' => 'text',
+  ),
+  2 => 
+  array (
+    'NAME' => 'Kids\'',
+    'CODE' => 'kids',
+    'SORT' => '500',
+    'ACTIVE' => 'Y',
+    'XML_ID' => NULL,
+    'DESCRIPTION' => '',
+    'DESCRIPTION_TYPE' => 'text',
+  ),
+)        );
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}

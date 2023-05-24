44a5540bba6910d0fe830e94b47586b4270f94f1
mkv173 <115578800+mkv173@users.noreply.github.com>
2023-05-24 14:29:26 +0400

    migration of information block sections and elements products

---
+++ local/php_interface/migrations/Version20230524132853.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Sprint\Migration;
+
+class Version20230524132853 extends Version
+{
+    protected $description   = "Элементы инфоблока товары";
+    protected $moduleVersion = "4.2.4";
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $this->getExchangeManager()
+             ->IblockElementsImport()
+             ->setExchangeResource('iblock_elements.xml')
+             ->setLimit(20)
+             ->execute(function ($item) {
+                 $this->getHelperManager()
+                      ->Iblock()
+                      ->addElement(
+                          $item['iblock_id'],
+                          $item['fields'],
+                          $item['properties']
+                      );
+             });
+    }
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function down()
+    {
+    }
+}
